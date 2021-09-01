57f75e090d42ff2eae75713c677cc9fb24c47998
Mishalzeera <78355230+Mishalzeera@users.noreply.github.com>
2021-09-01 12:30:53 +0200

    Header anim syntax in place

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,12 @@
+document.onload(headerAnim());
+
 function gsapTest() {
   gsap.to("#test", { opacity: 0, duration: 1, x: "-100vw", ease: "power2.in" });
+}
+
+function headerAnim() {
+  gsap.to("#h-white", { opacity: 0.3, duration: 4 });
+  gsap.to("#h-red", { opacity: 0.3, duration: 2 });
+  gsap.to("#h-green", { opacity: 0.3, duration: 1 });
+  gsap.to("#h-blue", { opacity: 0.3, duration: 3 });
 }
